588a0268171e66d3aec3c31a9d04741f48325d81
Florin Camarut <florin.camarut@student.usv.ro>
2024-03-03 00:04:37 +0200

    switch

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import useUsers from '@/hooks/useUsers';
+import useUsers from '@/hooks/useOrganisations';
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
