0c01afeeaf57bd7ad19fcc6389984526adefd6da
Florin Camarut <florin.camarut@student.usv.ro>
2024-03-05 20:19:09 +0200

    test:test

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -2,9 +2,12 @@
 
 import HeroSection from '@/components/landing/HeroSection';
 import MainNavigation from '@/components/navigation/MainNavigation';
+import useOrganisations from '@/hooks/useOrganisations';
 import React from 'react';
 
 const HomePage = () => {
+  const { data } = useOrganisations();
+  console.log(data);
   return (
     <>
       <MainNavigation />
@@ -12,5 +15,5 @@ const HomePage = () => {
     </>
   );
 };
-
+// test
 export default HomePage;
